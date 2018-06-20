f5ba853e7a642d643fe7491c41bacc9b7a5b38e5
Paul Kenneth Egell-Johnsen <paul.k.egell.johnsen@gmail.com>
2018-06-20 09:39:40 +0200

    Add files via upload

---
+++ src/components/mobxtest/MobxTest.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <p v-text="state.age"></p>
+        <p v-text="state.computedAge"></p>
+        <p v-for="user in state.users" :key="user.name">{{user.name}}</p>
+        <button @click="state.setAge()">increase age</button>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Observer } from "mobx-vue";
+import MobxTestModel from '~/components/mobxtest/MobxTestModel'
+
+@Observer
+@Component
+export default class MobxTest extends Vue {
+  state = new MobxTestModel();
+  mounted() {
+    this.state.fetchUsers();
+  }
+}
+</script>
---
+++ src/components/mobxtest/MobxTestModel.ts
@@ -0,0 +1,20 @@
+import { action, computed, observable } from "mobx";
+export default class MobxTestModel {
+  @observable age = 10;
+  @observable users = [];
+
+  @computed
+  get computedAge() {
+    return this.age + 1;
+  }
+
+  @action.bound
+  setAge() {
+    this.age++;
+  }
+
+  @action.bound
+  async fetchUsers() {
+    // this.users = [await http.get('/users')]
+  }
+}
